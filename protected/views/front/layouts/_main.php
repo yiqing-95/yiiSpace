d0f29f7f6c8019223c2e3c260d20bfb8930f6925
yiqing-95 <yiqing95@yahoo.com.cn>
2012-10-29 19:39:15 +0800

    添加换肤功能 和任务表 currentTask.md  以后当前正在进行的工作会表现在这里了

--- protected/views/front/layouts/_main.php
+++ protected/views/front/layouts/_main.php
@@ -3,8 +3,21 @@
 <head>
     <meta charset="utf-8">
     <title><?php echo CHtml::encode(Yii::app()->name); ?></title>
-    <?php WebUtil::registerRandomBootstrapTheme() ;?>
+    <?php //WebUtil::registerRandomBootstrapTheme() ;
+    // WebUtil::bootSwatch() ;
+    ?>
 
+    <link id="theme_style" rel="stylesheet"
+          href="" type="text/css"
+          media="screen"/>
+
+    <script type="text/javascript">
+        function changeTheme(ddlEle) {
+            //  alert($(ddlEle).val());
+            var themeStyle = "<?php echo Yii::app()->request->baseUrl; ?>/public/bootswatch2-0-4/{themeName}/bootstrap.min.css";
+            $("#theme_style").attr("href", themeStyle.replace("{themeName}", $(ddlEle).val()));
+        }
+    </script>
 </head>
 
 <body>
@@ -12,40 +25,60 @@
 <?php
 
 $topUserMenus = array();
-if(Yii::app()->user->isGuest){
-  $topUserMenus = array(
+if (Yii::app()->user->isGuest) {
+    $topUserMenus = array(
         array('label' => 'Home', 'url' => array('/site/index')),
-        array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), ),
+        array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"),),
         array('url' => Yii::app()->getModule('user')->registrationUrl, 'label' => Yii::app()->getModule('user')->t("Register"),),
         array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
     );
-}else{
+} else {
 
- $topUserMenus =  array(
-       array('label' =>  Yii::app()->user->name, 'url' =>  Yii::app()->getModule('user')->returnUrl,),
-       array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
-       array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
-   ) ;
+    $topUserMenus = array(
+        array('label' => Yii::app()->user->name, 'url' => Yii::app()->getModule('user')->returnUrl,),
+        array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
+        array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
+    );
+}
+
+//换肤实现
+$themesDir = Yii::getPathOfAlias('webroot.public.bootswatch2-0-4');
+$di = new DirectoryIterator($themesDir);
+$themes = array();
+foreach ($di as $d) {
+    if (!$d->isDot()) {
+        if ($d->getFilename() !== 'img') {
+            $themes[] = $d->getFilename();
+        }
+
+    }
 }
 
 
-$this->widget('bootstrap.widgets.TbNavbar',array(
-    'type'=>'inverse',
-    'brand'=>CHtml::encode(Yii::app()->name),
-    'collapse'=>true,
-    'items'=>array(
+
+
+
+$this->widget('bootstrap.widgets.TbNavbar', array(
+    //'type'=>'inverse',
+    'brand' => CHtml::encode(Yii::app()->name),
+    'collapse' => true,
+    'items' => array(
         array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'items'=>array(
-                array('label'=>'index', 'url'=>Yii::app()->homeUrl,
-                    'active'=>Yii::app()->controller->id === 'site' && Yii::app()->controller->action->id === 'index'),
-                array('label'=>'user', 'url'=>array('/user/user/index'),
-                    'active'=>Yii::app()->controller->id === 'site' && Yii::app()->controller->action->id === 'index'),
-                array('label'=>'blog', 'url'=>Yii::app()->homeUrl,
-                    'active'=>Yii::app()->controller->id === 'blog' && Yii::app()->controller->action->id === 'index'),
+            'class' => 'bootstrap.widgets.TbMenu',
+            'items' => array(
+                array('url' =>array('/user/user/space','u'=>Yii::app()->user->isGuest?0:Yii::app()->user->getId()), 'label' => '个人首页',
+                     'visible' => !Yii::app()->user->isGuest),
+                array('label' => 'user', 'url' => array('/user/user/index'),
+                    'active' => Yii::app()->controller->id === 'site' && Yii::app()->controller->action->id === 'index'),
+                array('label' => 'blog', 'url' => Yii::app()->homeUrl,
+                    'active' => Yii::app()->controller->id === 'blog' && Yii::app()->controller->action->id === 'index'),
+
             ),
-            'htmlOptions'=>array('class'=>'pull-left'),
+            'htmlOptions' => array('class' => 'pull-left'),
         ),
+        '<form class="navbar-search pull-left" action="">' .
+            CHtml::dropDownList('chang_theme', '', array_combine($themes, $themes), array('onchange' => 'changeTheme(this)'))
+            . '</form>',
         '<div class="add-this pull-right">
                         <!-- AddThis Button BEGIN -->
                         <div class="addthis_toolbox addthis_default_style">
@@ -59,9 +92,9 @@ $this->widget('bootstrap.widgets.TbNavbar',array(
                         <!-- AddThis Button END -->
                 </div>',
         array(
-            'class'=>'bootstrap.widgets.TbMenu',
-            'items'=>$topUserMenus,
-            'htmlOptions'=>array('class'=>'pull-right'),
+            'class' => 'bootstrap.widgets.TbMenu',
+            'items' => $topUserMenus,
+            'htmlOptions' => array('class' => 'pull-right'),
         ),
     ),
 )); ?>
@@ -69,15 +102,18 @@ $this->widget('bootstrap.widgets.TbNavbar',array(
 
 </div>
 <!--    主布局不做宽度限制 交由下面的子布局决定-->
-    <?php echo $content; ?>
+<?php echo $content; ?>
 
-<hr />
+<hr/>
 <div class="row" style="text-align: center;">
     <footer>
         <p class="powered">
-            Powered by <?php echo CHtml::link('Yii PHP framework', 'http://www.yiiframework.com', array('target'=>'_blank')); ?> /
-            <?php echo CHtml::link('jQuery', 'http://www.jquery.com', array('target'=>'_blank')); ?> /
-            <?php echo CHtml::link('YiiBooster', 'http://yii-booster.clevertech.biz/', array('target'=>'_blank')); ?> /
+            Powered
+            by <?php echo CHtml::link('Yii PHP framework', 'http://www.yiiframework.com', array('target' => '_blank')); ?>
+            /
+            <?php echo CHtml::link('jQuery', 'http://www.jquery.com', array('target' => '_blank')); ?> /
+            <?php echo CHtml::link('YiiBooster', 'http://yii-booster.clevertech.biz/', array('target' => '_blank')); ?>
+            /
         </p>
 
         <p class="copy">
