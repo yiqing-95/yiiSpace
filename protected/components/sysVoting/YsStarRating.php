1af1ebe853e082e663260f5f12e3ae9b3353285c
yiqing-95 <yiqing95@yahoo.com.cn>
2012-12-05 11:52:53 +0800

    想引入环境标识类 来标识开发阶段 和 开发状态所处的模式（是否在debug模式...  ）但还没有想到好的方案 先占位引入一个类以后再说..

---
+++ protected/components/sysVoting/YsStarRating.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yiqing
+ * Date: 12-12-5
+ * Time: 上午10:24
+ * To change this template use File | Settings | File Templates.
+ */
+class YsStarRating extends CStarRating
+{
+
+}
